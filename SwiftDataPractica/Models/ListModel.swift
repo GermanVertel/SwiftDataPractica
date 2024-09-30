749d9f589474cb333e78df99e36d0fcc1411e864
GermanVertel <84603164+GermanVertel@users.noreply.github.com>
2024-09-29 20:33:13 -0500

    creacion de la vista para agregar y guardar datos

---
+++ SwiftDataPractica/Models/ListModel.swift
@@ -0,0 +1,8 @@
+//
+//  ListModel.swift
+//  SwiftDataPractica
+//
+//  Created by German David Vertel Narvaez on 29/09/24.
+//
+
+import Foundation
---
+++ SwiftDataPractica/Views/addView.swift
@@ -0,0 +1,18 @@
+//
+//  addView.swift
+//  SwiftDataPractica
+//
+//  Created by German David Vertel Narvaez on 29/09/24.
+//
+
+import SwiftUI
+
+struct addView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    addView()
+}
