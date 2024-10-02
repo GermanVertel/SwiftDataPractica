3ec0523cac652f14206f5462a78766a418678166
GermanVertel <84603164+GermanVertel@users.noreply.github.com>
2024-10-02 11:26:23 -0500

    se creo una vista llamada comprasview donde vamos agregar al formaluario diferentes articulos a la lista de compras

---
+++ SwiftDataPractica/Views/ComprasView.swift
@@ -0,0 +1,18 @@
+//
+//  ComprasView.swift
+//  SwiftDataPractica
+//
+//  Created by German David Vertel Narvaez on 2/10/24.
+//
+
+import SwiftUI
+
+struct ComprasView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ComprasView()
+}
