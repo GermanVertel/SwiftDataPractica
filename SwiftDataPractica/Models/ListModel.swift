66679cc48c12ca3941d833661b7023c41f05f3d4
GermanVertel <84603164+GermanVertel@users.noreply.github.com>
2024-10-04 09:47:43 -0500

    se crea el modelo de datos de los arciculos y se hace la relacin con listmodel

--- SwiftDataPractica/Models/ListModel.swift
+++ SwiftDataPractica/Models/ListModel.swift
@@ -11,18 +11,25 @@ import SwiftData
 // modelo de la base de datos, funcional como el modelo relacinal de una BD
 @Model
 final class ListModel {
+    @Attribute(.unique) var id: String
     var titulo : String
     var fecha : Date
     var completado : Bool
     var presupuesto : String
     var total : Float
     
-    init(titulo: String = "", fecha: Date = .now, completado: Bool = false, presupuesto: String = "", total: Float = 0) {
+    
+    @Relationship(deleteRule: .cascade)
+    var relationArticulos: [ArticulosModel]
+    
+    init(id: String = UUID().uuidString, titulo: String = "", fecha: Date = .now, completado: Bool = false, presupuesto: String = "", total: Float = 0, relationArticulos: [ArticulosModel] = []) {
+        self.id = id
         self.titulo = titulo
         self.fecha = fecha
         self.completado = completado
         self.presupuesto = presupuesto
         self.total = total
+        self.relationArticulos = relationArticulos
     }
 }
 
